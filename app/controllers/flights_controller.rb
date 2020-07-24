602c7f8dd926f148c2adc391de251ca27a124f52
pudu87 <sam.cappelle87@gmail.com>
2020-07-24 19:17:17 -0400

    add flight selection to index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,9 +5,11 @@ class FlightsController < ApplicationController
     @airports_array = Airport.all.map { |airport| [airport.location, airport.id] }
     @months_array = Flight.all.map { |flight| flight.departure.month }.uniq.sort
     @days_array = Flight.all.map { |flight| flight.departure.day }.uniq.sort
-  end
 
-  def new
+    @available_flights = @flights.where(from_airport_id: params[:from_airport_id])
+                                 .where(to_airport_id: params[:to_airport_id])
+                                 .search_by_month(params[:month])
+                                 .search_by_day(params[:day])
   end
 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,9 @@ class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: 'Airport'
   belongs_to :to_airport,   class_name: 'Airport'
 
+  scope :search_by_month, ->(m) { where("cast(strftime('%m', departure) as int) = ?", m) }
+  scope :search_by_day,   ->(d) { where("cast(strftime('%d', departure) as int) = ?", d) }
+
   def duration=(time)
     if time.is_a?(String) && time.match(/[0-2]\d:[0-6]\d/)
       time = time[0..1].to_i*3600 + time[3..4].to_i*60
@@ -13,4 +16,5 @@ class Flight < ApplicationRecord
   def departure_formatted
     departure.strftime("%m/%d")
   end
+
 end
