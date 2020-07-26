858e13c53ae2ed2a4beed35d7fc54bef24d30163
pudu87 <sam.cappelle87@gmail.com>
2020-07-26 17:49:32 -0400

    create and migrate passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_211825) do
+ActiveRecord::Schema.define(version: 2020_07_26_214752) do
 
   create_table "airports", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2020_07_26_211825) do
     t.integer "duration"
     t.index ["from_airport_id"], name: "index_flights_on_from_airport_id"
     t.index ["to_airport_id"], name: "index_flights_on_to_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
 end
