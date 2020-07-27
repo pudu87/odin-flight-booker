e59e2bb93053318f232a8c0a548df4e6e0bad405
pudu87 <sam.cappelle87@gmail.com>
2020-07-27 17:26:50 -0400

    add create action to Bookingscontroller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,26 @@
 class BookingsController < ApplicationController
 
   def new
-    @flight = Flight.find(params[:flight_selection])
+    @flight = Flight.find(params[:flight_id])
     @booking = Booking.new
     @passenger = Passenger.new
-    params[:num_passengers].to_i.times { |t| @booking.passengers.build }
+    params[:num_passengers].to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to root_path
+    else
+      render new_booking_path
+    end   
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, :num_passengers,
+      passengers_attributes: [:id, :name, :email])
   end
 
 end
