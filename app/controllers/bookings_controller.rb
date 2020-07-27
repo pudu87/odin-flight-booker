c3fd11675d4927d4ec583bde0ddf1467f7b57e38
pudu87 <sam.cappelle87@gmail.com>
2020-07-27 14:51:09 -0400

    create Booking new-action and -view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,10 @@
 class BookingsController < ApplicationController
+
+  def new
+    @flight = Flight.find(params[:flight_selection])
+    @booking = Booking.new
+    @passenger = Passenger.new
+    params[:num_passengers].to_i.times { |t| @booking.passengers.build }
+  end
+
 end
